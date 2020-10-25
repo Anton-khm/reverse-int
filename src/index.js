e17f11956aeba3521219edac1593d2b3d3a28f2c
Anton <70846201+Anton-khm@users.noreply.github.com>
2020-10-25 23:24:12 +0300

    feat: implement reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports =  function reverseNum(num) {
+  let x = Math.pow(10, (num + '').length - 1);
+  while (num > 0) return (num % 10) * x + reverseNum(Math.floor(num / 10));
+  return 0;
+  } 
